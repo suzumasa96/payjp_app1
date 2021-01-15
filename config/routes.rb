e7110f60993a0c553e1bc396906dc73bcb09334b
suzumasa96 <73861844+suzumasa96@users.noreply.github.com>
2021-01-15 11:40:29 +0900

    カード登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'cards/new'
   get 'users/show'
   get 'items/index'
   devise_for :users
   root 'items#index'
   resources :users, only: [:show, :update]
+  resources :cards, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
